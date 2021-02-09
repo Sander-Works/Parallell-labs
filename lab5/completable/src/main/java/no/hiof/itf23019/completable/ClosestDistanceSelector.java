46a718693060a72d245a1d398892ac4451906b15
An Ngoc Lam <an.n.lam@hiof.no>
2021-02-09 23:28:07 +0100

    Update ClosestDistanceSelector.java

--- lab5/completable/src/main/java/no/hiof/itf23019/completable/ClosestDistanceSelector.java
+++ lab5/completable/src/main/java/no/hiof/itf23019/completable/ClosestDistanceSelector.java
@@ -10,17 +10,9 @@ public class ClosestDistanceSelector implements BiFunction<List<Long>, Long, Lon
 	public Long apply(List<Long> t, Long u) {
 		
 		//TODO: implement the function that return the number in the list t that is closed to the pivot u
+		//Hint: look at the implementation of step1Future in the Main
 		System.out.printf("%s: Step 1: Start\n", Thread.currentThread().getName());
-		long selected = 0;
-		long selectedDistance = Long.MAX_VALUE;
-		long distance;
-		for (Long number : t) {
-			distance = Math.abs(number - u);
-			if (distance < selectedDistance) {
-				selected = number;
-				selectedDistance = distance;
-			}
-		}
+
 		
 		System.out.printf("%s: Step 1: Result - %d\n", Thread.currentThread().getName(), selected);
 		
